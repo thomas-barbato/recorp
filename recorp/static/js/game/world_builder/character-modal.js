6f61dd8fa468f95a4d5c78550203bce0fc1d8a80
Case Gibson <t.barbato78@gmail.com>
2023-12-20 16:26:28 +0100

    add:skill description, discard change, button style

--- recorp/static/js/game/world_builder/character-modal.js
+++ recorp/static/js/game/world_builder/character-modal.js
@@ -1,7 +1,9 @@
-let user_description = document.querySelector('#usr-background');
+const user_description = document.querySelector('#usr-background');
 let user_description_editor = "";
 let editor_btn = document.querySelector('#edit-mod-btn');
-let close_editor_btn = document.querySelector('#cancel-edit-mod-btn');
+let btn_group = document.querySelector('#btn-group');
+let save_change_btn = document.querySelector('#save-edition-btn')
+let discard_change_btn = document.querySelector('#cancel-edition-btn');
 const reader = new FileReader();
 const img_user = document.querySelector('#user-avatar');
 const img_input = document.querySelector('.hidden-input-file');
@@ -30,19 +32,28 @@ editor_btn.addEventListener('click', function(){
         console.error( error );
     });
 
-    close_editor_btn.classList.remove('hidden');
+    btn_group.classList.remove('hidden');
     editor_btn.classList.add('hidden');
 });
 
-close_editor_btn.addEventListener('click', function(){
-    const data = window.user_description_editor.getData();
-    console.log(data)
+function destroy_editor(){
     window.user_description_editor.destroy().catch( error => {
         console.log( error );
     } );
-    user_description.innerHTML = data;
+    btn_group.classList.add('hidden');
     editor_btn.classList.remove('hidden');
-    close_editor_btn.classList.add('hidden');
+}
+
+save_change_btn.addEventListener('click', function(){
+    // TODO: ADD SAVE
+    const data = window.user_description_editor.getData();
+    destroy_editor();
+    user_description.innerHTML = data;
+})
+
+discard_change_btn.addEventListener('click', function(){
+    destroy_editor();
+    user_description.innerHTML = JSON.parse(document.getElementById('description').textContent);
 })
 
 // END OF CHARACTER BACKGROUND EDITOR
@@ -63,3 +74,21 @@ img_input.addEventListener('change', e => {
 })
 
 // END OF CHANGE CHARACTER IMG ZONE
+
+// DISPLAY SKILL DESCRIPTION ON PHONE ZONE
+
+let skill = document.querySelectorAll('.skills')
+
+for(let i = 0; i < skill.length ; i++){
+    let id = skill[i].id.split('skill-')[1];
+    let descr_elem = document.querySelector('#skill-descr-'+id);
+    skill[i].addEventListener('click', function(){
+        if(descr_elem.classList.contains("hidden")) {
+            descr_elem.classList.remove('hidden');
+        }else{
+            descr_elem.classList.add('hidden');
+        }
+    })
+}
+
+// END OF SKILL DESCRIPTION ON PHONE ZONE
