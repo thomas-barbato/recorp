8001cf9ea844b7d5d50d5b1c183ad9fb7d25d9ab
Case Gibson <t.barbato78@gmail.com>
2024-04-01 15:15:32 +0200

    feat: fix ship border color, start modal

--- recorp/static/js/game/world_builder/game.js
+++ recorp/static/js/game/world_builder/game.js
@@ -35,13 +35,17 @@ function add_sector_foreground(sector_element){
     let animation_type = "";
     let data_i = 1;
     for(var [key, value] in sector_element){
+        console.log(sector_element)
         animation_dir_data.push(sector_element[key]["animations"]);
         animation_type = sector_element[key]["type"];
         let cell = 0;
-        let coord_x = parseInt(sector_element[key]["data"]["coord_x"]);
+        let coord_x = parseInt(sector_element[key]["data"]["coord_x"]) ;
         let coord_y = parseInt(sector_element[key]["data"]["coord_y"]);
         let size_x = parseInt(sector_element[key]["size"]["size_x"]);
         let size_y = parseInt(sector_element[key]["size"]["size_y"]);
+        let element_data = sector_element[key]["data"];
+        let modal = create_modal(element_data["name"]);
+        document.querySelector('#modal-container').append(modal);
         for(let row = sector_element[key]["data"]["coord_y"]; row < coord_y + size_y; row++){
             for(let col = sector_element[key]["data"]['coord_x']; col < coord_x + size_x; col++){
                 let entry_point = document.querySelector('.tabletop-view').rows[row].cells[col];
@@ -58,7 +62,8 @@ function add_sector_foreground(sector_element){
                     row,
                     col,
                     sector_element[key]["size"]['size_x'],
-                    sector_element[key]["size"]['size_y']
+                    sector_element[key]["size"]['size_y'],
+                    element_data
                 );
                 cell++;
             }
@@ -67,6 +72,92 @@ function add_sector_foreground(sector_element){
         cell = 0;
         data_i++;
     }
+}
+
+function add_foreground_tiles(anim_type, anim_array, cell, row, col, size_x, size_y, element_data){
+    console.log(element_data)
+    let temporary_array = [];
+    for(let i = 0; i < anim_array.length; i++){
+        let animation_i = 0;
+        for(let [k, v] in anim_array[i]){
+            let fg_animation = document.createElement('img');
+            let fg_animation_url = '/static/img/atlas/foreground/' + anim_type + '/' + anim_array[i][k] + '/' + cell + '.png';
+            fg_animation.setAttribute('data-modal-target', "modal-" + element_data["name"]);
+            fg_animation.setAttribute('data-modal-toggle', "modal-" + element_data["name"]);
+            fg_animation.setAttribute('onclick', "open_close_modal('" + "modal-" + element_data["name"] + "')")
+            fg_animation.src = fg_animation_url;
+            fg_animation.classList.add('animation', 'z-2', 'absolute', 'm-auto', 'left-0', 'right-0', 'cursor-pointer', 'clickable', 'no-cross', "element");
+            if(size_y > 1 && size_y > 1){
+                fg_animation.style.display = "none";
+                fg_animation.classList.add('animation-'+animation_i,"clickable");
+            }else{
+                fg_animation.style.display = "block";
+            }
+            animation_set.add('.animation-'+animation_i);
+            let entry_point = document.querySelector('.tabletop-view').rows[row].cells[col];
+            entry_point.querySelector('div').style.borderColor = "orange";
+            entry_point.querySelector('div').append(fg_animation);
+            animation_i++;
+        }
+        animation_i=0;
+    }
+}
+
+function add_pc_npc(data){
+    let border_color = "";
+    for(let i = 0; i < data.length; i++){
+        let coord_x = (data[i]["coordinates"]["coord_x"]) + 1;
+        let coord_y = (data[i]["coordinates"]["coord_y"]) + 1;
+        console.log(coord_y, coord_x)
+        let entry_point = document.querySelector('.tabletop-view').rows[coord_y].cells[coord_x];
+        let div = entry_point.querySelector('div');
+        let pc_or_npc_class = data[i]["is_npc"] == true ? "npc" : "pc"
+        border_color = "lime";
+        if(data[i]["user_id"] != current_user_id && data[i]["is_npc"]){
+            border_color = "red";
+        }else if(data[i]["user_id"] != current_user_id && !data[i]["is_npc"]){
+            border_color = "cyan";
+        }
+
+        div.style.borderColor = border_color;
+
+        space_ship = document.createElement('img');
+        space_ship.src = "/static/js/game/assets/ships/ship01-32px.png";
+        space_ship.classList.add('w-[32px]', 'h-[32px]', 'cursor-pointer', 'clickable', 'no-cross', pc_or_npc_class);
+        div.append(space_ship);
+    }
+}
+
+function create_modal(id){
+    let e = document.createElement('div');
+    e.id = "modal-" + id;
+    e.setAttribute('aria-hidden', true);
+    e.setAttribute('tabindex', -1);
+    e.classList.add(
+        'hidden',
+        'overflow-y-auto',
+        'overflow-x-hidden',
+        'fixed',
+        'top-0',
+        'right-0',
+        'left-0',
+        'z-50',
+        'justify-center',
+        'items-center',
+        'w-full',
+        'h-full',
+        'md:inset-0',
+        'backdrop-blur-sm',
+        'bg-black/20',
+        'border-1'
+    );
+    e.textContent = id;
+    return e;
+}
+
+function open_close_modal(id){
+    let e = document.querySelector('#'+id)
+    e.classList.contains("hidden") == true ? e.classList.remove('hidden') : e.classList.add('hidden');
 }
 
 function display_animation(timer="500"){
@@ -89,56 +180,8 @@ function display_animation(timer="500"){
     }, timer);
 }
 
-function add_foreground_tiles(anim_type, anim_array, cell, row, col, size_x, size_y){
-    let temporary_array = [];
-    for(let i = 0; i < anim_array.length; i++){
-        let animation_i = 0;
-        for(let [k, v] in anim_array[i]){
-            let fg_animation = document.createElement('img');
-            let fg_animation_url = '/static/img/atlas/foreground/' + anim_type + '/' + anim_array[i][k] + '/' + cell + '.png';
-            fg_animation.src = fg_animation_url;
-            fg_animation.classList.add('animation', 'z-2', 'absolute', 'm-auto', 'left-0', 'right-0', 'cursor-pointer');
-            if(size_y > 1 && size_y > 1){
-                fg_animation.style.display = "none";
-                fg_animation.classList.add('animation-'+animation_i);
-            }else{
-                fg_animation.style.display = "block";
-            }
-            animation_set.add('.animation-'+animation_i);
-            let entry_point = document.querySelector('.tabletop-view').rows[row].cells[col];
-            entry_point.querySelector('div').classList.add('hover:border-amber-400');
-            entry_point.querySelector('div').append(fg_animation);
-            animation_i++;
-        }
-        animation_i=0;
-    }
-}
 
-function add_pc_npc(data){
-    let border_color = "";
-    for(let i = 0; i < data.length; i++){
-        let coord_x = (data[i]["coordinates"]["coord_x"]) + 1;
-        let coord_y = (data[i]["coordinates"]["coord_y"]) + 1;
-        let entry_point = document.querySelector('.tabletop-view').rows[coord_y].cells[coord_x];
-        let div = entry_point.querySelector('div');
-
-        if(data[i]["user_id"] == current_user_id){
-            border_color = "hover:border-emerald-500";
-        }else{
-            border_color = data[i]["is_npc"] !== true ? "border-neutral-600" : "border-cyan-400";
-        }
-
-        div.classList.add(border_color);
-
-        space_ship = document.createElement('img');
-        space_ship.src = "/static/js/game/assets/ships/ship01-32px.png";
-        space_ship.classList.add('w-[32px]', 'h-[32px]', 'cursor-pointer');
-        div.append(space_ship);
-    }
-}
-
-window.addEventListener('load', function(event) {
-
+window.addEventListener('load', () => {
     let room = map_informations.sector.id;
     let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var gameSocket = new WebSocket(
@@ -153,7 +196,6 @@ window.addEventListener('load', function(event) {
     gameSocket.onopen = function(e){
         console.log("socket opened")
     };
-
 
     gameSocket.onclose = function(e) {
         console.log("WebSocket connection closed unexpectedly. Trying to reconnect in 1s...");
