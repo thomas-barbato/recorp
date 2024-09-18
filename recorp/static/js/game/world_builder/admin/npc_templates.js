192b7a3dc72421b6def899145155cba7e2df618a
Case Gibson <t.barbato78@gmail.com>
2024-09-18 16:26:18 +0200

    feat: working on npc template creation

--- recorp/static/js/game/world_builder/admin/npc_templates.js
+++ recorp/static/js/game/world_builder/admin/npc_templates.js
@@ -4,13 +4,17 @@ const ship_img = document.querySelector('#spaceship-img');
 let resources_values = document.querySelectorAll('.resources');
 let module_select = document.querySelectorAll('.module-multi-select');
 let spaceship_description_element = document.querySelectorAll('.description-spaceship')
+let selected_spaceship_name = "";
 let dict = [];
+
+
+let submit_button = document.querySelector('#npc-create-btn');
+let cancel_button = document.querySelector('#npc-cancel-btn');
 
 // CHANGE CHARACTER IMG ZONE
 ship_select.addEventListener('change', function() {
     let selected_element = ship_select.options[ship_select.selectedIndex];
     let select_ship_description_id = selected_element.dataset.shipid;
-    let select_ship_description = document.querySelector('#description-ship-' + select_ship_description_id);
     ship_img.src = "";
 
     if (selected_element.value != "none") {
@@ -50,7 +54,7 @@ npc_submit_button.addEventListener('click', function() {
     let template_name = document.querySelector('#template-name-input').value;
     let selected_ship = ship_select.options[ship_select.selectedIndex];
     let ship_id = selected_ship.value;
-    let ship_name = selected_ship.textContent;
+    selected_spaceship_name = selected_ship.textContent;
     let ship_image = document.querySelector('#spaceship-img').src.split('/')
     let ship_image_name = ship_image[ship_image.length - 1];
     let difficulty_input = document.querySelector('#difficulty-select');
@@ -113,11 +117,13 @@ npc_submit_button.addEventListener('click', function() {
         modal_spaceship.src = `/static/js/game/assets/ships/${ship_image_name}`;
         modal_spaceship.classList.remove('hidden');
         modal_spaceship_warning = document.getElementById('npc-template-modal-spaceship-warning').classList.add('hidden');
+        submit_button.disabled = false;
     } else {
         let modal_spaceship = document.getElementById('npc-template-modal-spaceship');
         modal_spaceship.src = "";
         modal_spaceship.classList.add('hidden');
         modal_spaceship_warning = document.getElementById('npc-template-modal-spaceship-warning').classList.remove('hidden');
+        submit_button.disabled = true;
     }
     if (template_name) {
         let template_name_element = document.getElementById('npc-template-modal-name')
@@ -128,9 +134,16 @@ npc_submit_button.addEventListener('click', function() {
     for (let skill in skill_info_array) {
         let skill_element_li = document.createElement('li');
         skill_element_li.textContent = `${skill_info_array[skill].name}: ${skill_info_array[skill].level}`;
-        skill_element_li.classList.add('list-none', 'bg-gray-600', 'border', 'border-gray-800', 'delete-after-cancel');
+        skill_element_li.classList.add('list-none', 'bg-gray-600', 'border', 'border-gray-800', 'delete-after-cancel', 'text-shadow');
         document.querySelector('#npc-template-modal-skills-ul').append(skill_element_li)
     }
+
+    let module_slot_avaiable_on_ship_element = document.getElementById(`${selected_spaceship_name}-module-slot-available`)
+    let module_slot_available_on_ship = "";
+    let module_element_span_moduleCount = document.querySelector('#npc-template-modal-module-moduleCount');
+    let module_element_span_moduleCount_warningMsg = document.querySelector('#npc-template-modal-module-moduleCount-warning');
+    let module_element_span_moduleCount_warningMsgValue = document.querySelector('#npc-template-modal-module-moduleCount-warning-value');
+    let module_element_span_spaceship_warningMsg = document.querySelector('#npc-template-modal-module-spaceship-warning');
 
     if (module_info_array.length > 0) {
         document.querySelector('#npc-template-modal-module-warning-li').classList.add('hidden');
@@ -138,23 +151,54 @@ npc_submit_button.addEventListener('click', function() {
             let module_element_li = document.createElement('li');
             let module_element_li_span_name = document.createElement('span');
             let module_element_li_span_id = document.createElement('span');
+            let module_element_li_span_effects = document.createElement('span');
 
-            module_element_li_span_id.classList.add('text-center', 'font-bold');
+            module_element_li_span_id.classList.add('text-center', 'font-bold', 'text-shadow');
             module_element_li_span_id.textContent = `id: ${module_info_array[module].id}`;
-            module_element_li_span_name.classList.add('text-center');
-            module_element_li_span_name.textContent = `${module_info_array[module].name}`;
+            module_element_li_span_name.classList.add('text-center', 'text-shadow');
+            module_element_li_span_name.textContent = `${module_info_array[module].name.split(' -')[0]}`;
+            module_element_li_span_effects.classList.add('text-center', 'text-shadow');
+            module_element_li_span_effects.textContent = `${module_info_array[module].effects}`;
 
-
-            module_element_li.classList.add('flex', 'flex-col', 'gap-1', 'text-justify');
+            module_element_li.classList.add('flex', 'flex-col', 'gap-1', 'text-justify', 'text-shadow');
             module_element_li.append(module_element_li_span_id);
             module_element_li.append(module_element_li_span_name);
+            module_element_li.append(module_element_li_span_effects);
 
             module_element_li.classList.add('list-none', 'bg-gray-600', 'border', 'border-gray-800', 'delete-after-cancel');
             document.querySelector('#npc-template-modal-module-ul').append(module_element_li)
         }
+        module_slot_available_on_ship = module_slot_avaiable_on_ship_element ? parseInt(document.getElementById(`${selected_spaceship_name}-module-slot-available`).textContent) : -1;
+        if (module_slot_avaiable_on_ship_element != -1) {
+            module_element_span_spaceship_warningMsg.classList.add('hidden');
+        } else {
+            module_element_span_spaceship_warningMsg.classList.remove('hidden');
+
+        }
+        module_element_span_moduleCount.textContent = `(${module_info_array.length} selected)`;
+        if (module_info_array.length <= module_slot_available_on_ship) {
+            module_element_span_moduleCount.classList.add('text-lime-400', 'text-shadow');
+            module_element_span_moduleCount.classList.remove('text-red-600', 'text-shadow');
+            module_element_span_moduleCount_warningMsg.classList.add('hidden');
+            submit_button.disabled = false;
+        } else {
+            module_element_span_moduleCount.classList.remove('text-lime-400', 'text-shadow');
+            module_element_span_moduleCount.classList.add('text-red-600', 'text-shadow');
+            module_element_span_moduleCount_warningMsgValue.textContent = module_slot_available_on_ship;
+            module_element_span_moduleCount_warningMsg.classList.remove('hidden');
+            submit_button.disabled = true;
+        }
     } else {
-        document.querySelector('#npc-template-modal-module-warning-li').classList.remove('hidden');
+        module_element_span_moduleCount.classList.remove('text-lime-400', 'text-shadow');
+        module_element_span_moduleCount.classList.add('text-red-600', 'text-shadow');
+        if (module_slot_available_on_ship == -1) {
+            module_element_span_moduleCount_warningMsg.classList.add('hidden');
+            module_element_span_spaceship_warningMsg.classList.remove('hidden');
+
+        }
+        submit_button.disabled = true;
     }
+
 
     if (resource_info_array.length > 0) {
         document.querySelector('#npc-template-modal-resource-warning-li').classList.add('hidden');
@@ -165,12 +209,12 @@ npc_submit_button.addEventListener('click', function() {
             let resource_element_li_span_canBeRandomized = document.createElement('span');
 
             resource_element_li.classList.add();
-            resource_element_li_span_id.classList.add('text-center', 'font-bold');
+            resource_element_li_span_id.classList.add('text-center', 'font-bold', 'text-shadow');
             resource_element_li_span_id.textContent = `id: ${resource_info_array[resource].id}`;
-            resource_element_li_span_name.classList.add('text-center');
+            resource_element_li_span_name.classList.add('text-center', 'text-shadow');
             resource_element_li_span_name.textContent = `${resource_info_array[resource].name} - quantity : ${resource_info_array[resource].quantity}`;
             resource_element_li_span_canBeRandomized.textContent = `Can be randomized: ${resource_info_array[resource].can_be_randomized}`;
-            resource_element_li_span_canBeRandomized.classList.add('text-center');
+            resource_element_li_span_canBeRandomized.classList.add('text-center', 'text-shadow');
 
 
             resource_element_li.classList.add('flex', 'flex-col', 'gap-1', 'text-justify', 'list-none', 'bg-gray-600', 'border', 'border-gray-800', 'delete-after-cancel');
@@ -185,14 +229,11 @@ npc_submit_button.addEventListener('click', function() {
     }
 
 
-    let submit_button = document.querySelector('#npc-create-btn');
     submit_button.addEventListener('click', function() {
         save_or_update_npc_template();
     })
 
-    let cancel_button = document.querySelector('#npc-cancel-btn');
     cancel_button.addEventListener('click', function() {
-        console.log("ok cancel_button");
         document.querySelectorAll('.delete-after-cancel').forEach(el => el.remove());
     })
 
